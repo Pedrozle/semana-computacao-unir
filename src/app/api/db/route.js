302b6ab45e6922ffb391650d205fe53c59f66c03
Pedrozle <pedrozle@outlook.com>
2023-05-20 22:42:05 -0400

    corrigido consulta banco

--- src/app/api/db/route.js
+++ src/app/api/db/route.js
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 import { MongoClient } from "mongodb";
 
-export async function GET(request, context) {
-    const colecao = context.params.colecao;
-    console.log(colecao);
+export async function GET(request) {
+    let url = request.url.split("?");
+    let query = url[1];
+    let queries = query.split("&");
+    let database = queries[0].split("=")[1];
+    let colecao = queries[1].split("=")[1];
 
     const client = new MongoClient(
         `mongodb+srv://pedrozle:7jaBI5gvZ0WZnoXK@cluster0.kunsu4f.mongodb.net/`,
@@ -15,7 +18,7 @@ export async function GET(request, context) {
     await client.connect();
 
     // Acesso ao banco de dados
-    const db = client.db("social");
+    const db = client.db(database);
     const collection = db.collection(colecao);
 
     const data = await collection.find().toArray();
