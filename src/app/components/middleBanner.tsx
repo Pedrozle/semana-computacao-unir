4d61ee6d111c8d6146b3816ef90505f8f22fa8d6
Pedrozle <pedrozle@outlook.com>
2023-05-27 21:05:50 -0400

    update

--- src/app/components/middleBanner.tsx
+++ src/app/components/middleBanner.tsx
@@ -9,16 +9,16 @@ export default function MiddleBanner() {
                 <p className={styles.title}>
                     Inscreva-se para a Semana da Computação
                 </p>
-                <div className={styles.botao_container}>
-                    <Button
-                        variant="primary"
-                        onClick={() =>
-                            (window.location.href = "https://www.eventbrite.com.br/e/vi-semana-da-computacao-tickets-643466023197")
-                        }
-                    >
-                        Quero Participar!
-                    </Button>
-                </div>
+                <Button
+                    className={styles.button}
+                    variant="primary"
+                    onClick={() =>
+                        (window.location.href =
+                            "https://www.eventbrite.com.br/e/vi-semana-da-computacao-tickets-643466023197")
+                    }
+                >
+                    Quero Participar!
+                </Button>
             </div>
         </div>
     );
