4c599aaa3d2a5b34421086a92e3711db9784f184
Pedrozle <pedrozle@outlook.com>
2023-06-02 21:48:50 -0400

    alterado estilos

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,38 +5,50 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Logo from "@/assets/img/logo1.png";
 import Button from "react-bootstrap/Button";
+import { Col, Row } from "react-bootstrap";
+
+import styles from "@/styles/components/header.module.scss";
 
 export default function Header() {
     return (
         <header>
-            <Navbar bg="dark" expand="lg" variant="dark">
+            <Navbar className={styles.header} expand="lg" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">
-                        <Image
-                            src={Logo}
-                            alt="Logo semana da computação"
-                            width={325}
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#inicio">Início</Nav.Link>
-                            <Nav.Link href="#sobre">Sobre</Nav.Link>
-                            <Nav.Link href="#palestras">Palestras</Nav.Link>
-                            <Nav.Link href="#minicursos">Minicursos</Nav.Link>
-                            <Nav.Link href="#trabalhos">Trabalhos</Nav.Link>
-                            <Button
-                                variant="primary"
-                                onClick={() =>
-                                    (window.location.href =
-                                        "https://www.eventbrite.com.br/e/vi-semana-da-computacao-tickets-643466023197")
-                                }
-                            >
-                                Inscreva-se!
-                            </Button>
-                        </Nav>
-                    </Navbar.Collapse>
+                    <Row className={styles.row}>
+                        <Col>
+                            <Navbar.Brand href="#home">
+                                <Image
+                                    src={Logo}
+                                    alt="Logo semana da computação"
+                                    width={325}
+                                />
+                            </Navbar.Brand>
+                        </Col>
+                        <Col>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link href="#inicio">Início</Nav.Link>
+                                    <Nav.Link href="#agenda">Agenda</Nav.Link>
+                                    <Nav.Link href="#paineis">
+                                        Sessão de painéis
+                                    </Nav.Link>
+                                    <Nav.Link href="#maratona">
+                                        Maratona de programação
+                                    </Nav.Link>
+                                    <Button
+                                        variant="primary"
+                                        onClick={() =>
+                                            (window.location.href =
+                                                "https://www.eventbrite.com.br/e/vi-semana-da-computacao-tickets-643466023197")
+                                        }
+                                    >
+                                        Inscreva-se!
+                                    </Button>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Col>
+                    </Row>
                 </Container>
             </Navbar>
         </header>
