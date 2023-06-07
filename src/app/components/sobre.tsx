978d9140e3f79379fd4c125a11f85c719163fdf1
Pedrozle <pedrozle@outlook.com>
2023-06-06 22:15:41 -0400

    update

--- src/app/components/sobre.tsx
+++ src/app/components/sobre.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "@/styles/components/sobre.module.scss";
-import primeiro_dia from "@/assets/img/primeiro_dia.jpeg";
+import primeiro_dia from "@/assets/img/sobre.jpeg";
 
 export default function Sobre() {
     return (
@@ -8,34 +8,67 @@ export default function Sobre() {
             <div className={styles.conteudo}>
                 <p className={styles.title}>Sobre a Semana da Computação</p>
                 <div className={styles.descricao}>
-                    <div>
-                        <p>
-                            A SEMANA DA COMPUTAÇÃO DA UNIR, antigamente
-                            denominada INFOUNIR (Semana de Informática), é um
-                            evento aberto a toda comunidade regional e de outros
-                            centros, procurando congregar estudantes de
+                    <div className={styles.info}>
+                        <p className={styles.texto}>
+                            É um evento aberto a toda comunidade regional e de
+                            outros centros, procurando congregar estudantes de
                             graduação e pós-graduação, professores,
                             pesquisadores, profissionais e demais interessados
                             em computação, objetivando a difusão da computação e
                             do seu bom uso, trazendo para o debate aberto, temas
-                            importantes relacionados à Computação e Inovação
-                            Tecnológica
+                            importantes relacionados à Ciência da Computação.
                         </p>
-                        <Image src={primeiro_dia} alt="" />
+                        <Image
+                            src={primeiro_dia}
+                            alt=""
+                            className={styles.img}
+                        />
                     </div>
-                    <div className={styles.texto}>
-                        <Image src={primeiro_dia} alt="" />
-                        <p>
-                            Nesta edição, o evento será integrado ao Workshop de
-                            Tecnologias de Redes do PoP-RO, voltado para
-                            gestores e equipes técnicas da área de TIC das
-                            principais instituições públicas e privadas de
-                            ensino e pesquisa do estado de Rondônia.
-                        </p>
+                    <div className={styles.info}>
+                        <Image
+                            src={primeiro_dia}
+                            alt=""
+                            className={`${styles.img} ${styles.img_2}`}
+                        />
+                        <div>
+                            <p className={styles.texto}>
+                                Além de propiciar uma troca de experiência, o
+                                objetivo também é gerar um ambiente para
+                                atualização tecnológica através de atividades
+                                complementares como palestras, debates e
+                                minicursos.
+                            </p>
+                            <p className={styles.texto}>
+                                Também haverá apresentação de trabalhos
+                                acadêmicos desenvolvidos pelos alunos do curso
+                                de Ciência da Computação da Unir, a fim de
+                                divulgar à comunidade as pesquisas desenvolvidas
+                                nesta temática pelos alunos e professores. As
+                                inscrições são limitadas para as atividades e é
+                                necessário que o participante escolha e se
+                                inscreva na(s) atividade(s) que irá participar.
+                            </p>
+                        </div>
                     </div>
-                    <div className={styles.foto}></div>
                 </div>
             </div>
         </div>
     );
 }
+
+// <div className={styles.info}>
+//                         <p className={styles.texto}>
+//                             É um evento aberto a toda comunidade regional e de
+//                             outros centros, procurando congregar estudantes de
+//                             graduação e pós-graduação, professores,
+//                             pesquisadores, profissionais e demais interessados
+//                             em computação, objetivando a difusão da computação e
+//                             do seu bom uso, trazendo para o debate aberto, temas
+//                             importantes relacionados à Ciência da Computação.
+//                         </p>
+//                         <Image
+//                             src={primeiro_dia}
+//                             alt=""
+//                             className={styles.img}
+//                         />
+//                     </div>
