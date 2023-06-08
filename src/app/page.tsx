368abfb53477ff518d2fb754b0833b54fa3023fa
Pedrozle <pedrozle@outlook.com>
2023-06-07 22:17:01 -0400

    adicionado  seção onde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Banner from "@/app/components/banner";
 import Atividades from "./components/atividades";
 import MiddleBanner from "./components/middleBanner";
 import Sobre from "./components/sobre";
+import Onde from "./components/onde";
 
 export default function Page() {
     return (
         <main>
             <Banner />
             <Sobre />
+            <Onde />
             <Atividades link="palestras" title="Palestras" />
             <MiddleBanner />
             <Atividades link="minicursos" title="Minicursos" />
